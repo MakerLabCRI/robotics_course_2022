4431f5b7e9a3d1b7925125b03dcf7dba5f0fcf93
[tiffany <tiffany2626lin@gmail.com>
2023-01-05 09:15:33 +0100

    Update final_project.ino

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -8,6 +8,21 @@
  * 
  */
 
+maze format
+7 _ _ _ _ _ _ _ _
+6 _ _ _ _ _ _ _ _
+5 _ _ _ _ _ _ _ _
+4 _ _ _ _ _ _ _ _
+3 _ _ _ _ _ _ _ _
+2 _ _ _ _ _ _ _ _
+1 _ _ _ _ _ _ _ _
+0 _ _ _ _ _ _ _ _
+  0 1 2 3 4 5 6 7
+start=0-0;
+arrival=7-7;
+*/
+
+
 // importing libraries 
 #include <Wire.h>
 #include <ZumoShield.h>
