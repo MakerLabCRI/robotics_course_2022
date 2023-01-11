7e2c340b4370abddbe57c69cbe2f0b97213a7c0c
[tiffany <tiffany2626lin@gmail.com>
2023-01-11 01:18:27 +0100

    Update final_project.ino

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -1,247 +1,218 @@
 /*
- * Pathfinding Pololu Zumo Robot with Obstacle Avoidance 
+ * Pololu Zumo Robot with Obstacle Avoidance 
  *
  * by Clemence, Romaric & Tiffany 
  *
- * This robot will find the shortest path from point A to B
- * in a 8x8 grid while avoiding known obstacles placed on the grid.
+ * This robot will travel from start to goal in a 8x8 grid while
+ * avoiding known obstacles placed on the grid.
  * 
  */
-
-/*
-maze format
-7 _ _ _ _ _ _ _ _
-6 _ _ _ _ _ _ _ _
-5 _ _ _ _ _ _ _ _
-4 _ _ _ _ _ _ _ _
-3 _ _ _ _ _ _ _ _
-2 _ _ _ _ _ _ _ _
-1 _ _ _ _ _ _ _ _
-0 _ _ _ _ _ _ _ _
-  0 1 2 3 4 5 6 7
-start=0-0;
-arrival=7-7;
-*/
-
-
-// importing libraries 
-//#include <Wire.h>
+// import library
 #include <ZumoShield.h>
-//#include <NewPing.h>
 
-//setting up Zumo robot
-ZumoBuzzer buzzer;
-//ZumoReflectanceSensorArray reflectanceSensors;
+//Zumo robot set-up
 ZumoMotors motors;
-//Pushbutton button(ZUMO_BUTTON);
-int lastError = 0;
 
-//defining motors speed and duration 
-#define REVERSE_SPEED 200 
-#define TURN_SPEED 200
-#define FORWARD_SPEED 100
-#define REVERSE_DURATION 500 
-#define TURN_DURATION 500
-#define FORWARD_DURATION 5000
-#define STOP 0 
-#define STOP_DURATION 100 
+//define constants
+#define PATH_LEN 15
+#define X 8
+#define Y 8
 
-/*setting up ultrasound sensor pins and max distance 
-#define TRIGGER_PIN  12 
-#define ECHO_PIN     11  
-#define MAX_DISTANCE 200
+//motor speeds and duration 
+#define TURN_SPEED 275
+#define FORWARD_SPEED 200
+#define FORWARD_DURATION 1600 
+#define TURN_LEFT_DURATION 250 
+#define TURN_RIGHT_DURATION 246
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-
-//setting up time constants for avoiding obstacles
-#define OBSTACLE_CHECK_INTERVAL 500 // check for obstacles every 500 ms
-#define OBSTACLE_AVOID_DURATION 200 // avoid obstacles for 2 sec
-*/
-
-// variables for wavefront
-int pathway = 0;
-int obstacle = 255;
-int goal = 1;
-int robot = 254;
-
-//starting robot/goal locations
-int robot_x = 7;
-int robot_y = 0;
-int goal_x = 0;
-int goal_y = 7;
-
-// grid locations
-int x=7;
-int y=0;
-
-//temporary variables
-int counter=0;
-
-//for looking for smallest node number
-int minimum_node=250;
-int min_node_location=0;
-int reset_min=250;//
-
-//defining grid for pathfinding 
-
-int grid[8][8]=	{{1,0,0,0,255,0,0,255},
-				 {0,0,255,0,0,0,0,0},
-				 {0,0,0,0,0,255,0,0},
-				 {0,255,0,255,0,0,0,255},
-				 {0,0,0,0,0,0,255,0},
-				 {0,255,0,255,255,0,0,0},
+//define 
+char position;
+//8x8 grid with obstacles = 1
+int grid[X][Y]=	{
+         {0,0,0,0,1,0,0,1},
+				 {0,0,1,0,0,0,0,0},
+				 {0,0,0,0,0,1,0,0},
+				 {0,1,0,1,0,0,0,1},
+				 {0,0,0,0,0,0,1,0},
+				 {0,1,0,1,1,0,0,0},
          {0,0,0,0,0,0,0,0},
-         {0,0,0,255,255,0,0,254},
+         {0,0,0,1,1,0,0,0},
          };
 
-/********************functions*******************/
-
-int propagate_wavefront(int robot_x, int robot_y, int goal_x, int goal_y)
-	{
-	//clear old wavefront
-	unpropagate(robot_x, robot_y);
-	
-	counter=0;//reset the counter for each run!
-    while(counter<50)//allows for recycling until robot is found
-        {
-        x=0;
-        y=0;
-    	while(x<8 && y<8)//while the grid hasnt been fully scanned
-    		{
-    		//if this location is a wall or the goal, just ignore it
-    		if (grid[x][y] != obstacle && grid[x][y] != goal)
-    			{	
-    			//a full trail to the robot has been located, finished!
-    			if (min_surrounding_node_value(x, y) < reset_min && grid[x][y]==robot)
-    				{
-    				//finshed! tell robot to start moving down path
-    				return min_node_location;
-    				}
-    			//record a value in to this node
-    			else if (minimum_node!=reset_min)//if this isnt here, 'nothing' will go in the location
-    			    grid[x][y]= minimum_node + 1;
-    			}
-    		
-    		//go to next node and/or row
-    		y++;
-    		if (y==6 && x!=6)
-    			{
-    			x++;
-    			y=0;
-    			}
-    		}
-   		counter++;
-        }
-    return 0;
-	}
-
-void unpropagate(int robot_x, int robot_y)//clears old path to determine new path
-	{
-	//stay within boundary
-	for(x=0; x<6; x++)
-		for(y=0; y<6; y++)
-			if (grid[x][y] != obstacle && grid[x][y] != goal) //if this location is something, just ignore it
-				grid[x][y] = pathway;//clear that space
-	
-	//store robot location in grid
-	grid[robot_x][robot_y]=robot;
-	//store robot location in grid
-	grid[goal_x][goal_y]=goal;
-	}
-  
-
-//if no solution is found, delete all obstacles from grid
-void clear_grid(void)
-	{	
-	for(x=0;x<6;x++)
-		for(y=0;y<6;y++)
-			if (grid[x][y] != robot && grid[x][y] != goal)
-				grid[x][y]=pathway;
-	}
-
-
-//this function looks at a node and returns the lowest value around that node
-//1 is up, 2 is right, 3 is down, and 4 is left (clockwise)
-int min_surrounding_node_value(int x, int y)
-	{
-	minimum_node=reset_min;//reset minimum
-
-	//down
-	if(x < 8)//not out of boundary
-		if  (grid[x+1][y] < minimum_node && grid[x+1][y] == pathway)//find the lowest number node, and exclude empty nodes (0's)
-		    {
-			minimum_node = grid[x+1][y];
-			min_node_location=3;
-            }
-
-	//up
-	if(x > 0)
-		if  (grid[x-1][y] < minimum_node && grid[x-1][y] == pathway)
-		    {
-			minimum_node = grid[x-1][y];
-			min_node_location=1;
-            }
-	
-	//right
-	if(y < 8)
-		if  (grid[x][y+1] < minimum_node && grid[x][y+1] == pathway)
-		    {
-			minimum_node = grid[x][y+1];
-			min_node_location=2;
-            }
-            
-	//left
-	if(y > 0)
-		if  (grid[x][y-1] < minimum_node && grid[x][y-1] == pathway)
-		    {
-			minimum_node = grid[x][y-1];
-			min_node_location=4;
-            }
-
-	   
-	return minimum_node;
-	}
-
-
 void setup() {
-  Serial.begin(19200);
+  // put your setup code here, to run once:
+  position = 'n';
+  Serial.begin(9600);
 }
 
-void loop() {
-  // Propagate the wavefront from the start location
-  int next_direction = propagate_wavefront(robot_x, robot_y, goal_x, goal_y);
-  Serial.println('hi');
-  // move the robot in the appropriate direction
+//robot path from start to goal
+int path[PATH_LEN][2] = {
+  {0,0},
+  {0,1},
+  {1,1},
+  {1,2},
+  {2,2},
+  {2,3},
+  {2,4},
+  {3,4},
+  {3,5},
+  {4,5},
+  {5,5},
+  {6,5},
+  {6,6},
+  {6,7},
+  {7,7}
+};
 
-  if (next_direction == 1) {
-    //move the robot up
-    //Serial.print(next_direction);
+/*
+goPath,
+This function moves the robot to the next location in the path.
+inputs = currentX: x current location, currentY: y current location, nextX: next x location, nextY: next y location 
+output = status of our followed path (3 for goal reached, 2 if obstacle detected, 1 for no direction 
+
+*/
+int goPath(int currentX,int currentY,int nextX,int nextY ){
+    
+    int returnValue = 1;
+
+    Serial.print("(");
+    Serial.print(currentX);
+    Serial.print(",");
+    Serial.print(currentY);
+    Serial.print(")");
+    Serial.print("-->");
+ 
+    Serial.print("(");
+    Serial.print(nextX);
+    Serial.print(",");
+    Serial.print(nextY);
+    Serial.println(")");
+
+    char direction = goNext(currentX, currentY, nextX, nextY);
+    Serial.print("Direction: ");
+
+    Serial.println(direction);
+
+    if (direction == 'r'){
+        turnRight();
+        goForward();
+    }
+    else if (direction == 'l'){
+        turnLeft();
+        goForward();
+    }
+
+    else if (direction == 'f'){
+        goForward();
+    }
+  return returnValue;
+
+}
+
+/*
+goNext,
+Depending on the current position, the robot moves to the next square on the grid.
+input = x: x current, y: y current, w: x next, z: y next
+output = f: front, b:back, l:left, r:right
+*/
+char goNext(int x,int y,int w,int z){
+
+    if (x == w && y < z){
+        if (position == 'n'){
+        return 'f';
+        }
+    else if (position == 'o'){return 'r';}
+    else if(position == 's'){return 'b';}
+    else if(position == 'e'){return 'l';}
+    }
+    if (x == w && y > z){
+        if (position == 'n'){
+        return 'b';}
+    else if (position == 'o'){return 'l';}
+    else if(position == 's'){return 'f';}
+    else if(position == 'e'){return 'r';}
+    }
+ if (x < w && y == z){
+        if (position == 'n'){
+        return 'l';}
+    else if (position == 'o'){return 'f';}
+    else if(position == 's'){return 'r';}
+    else if(position == 'e'){return 'b';}
+    }
+    if (x > w && y == z){
+        if (position == 'n'){
+        return 'r';}
+    else if (position == 'o'){return 'b';}
+    else if(position == 's'){return 'l';}
+    else if(position == 'e'){return 'f';}
+    }
+    if ((x == w && y == z) || (x !=w && y != z)){
+        return 'e';
+    }
+
+}
+
+//functions to move robot
+//robot moves forward
+void goForward(){
     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-    //Serial.println('move the robot up');
-     //delay(FORWARD_DURATION);
-   } 
+    delay(FORWARD_DURATION);
+    stop();
+}
 
-   else if (next_direction == 2) {
-    // move the robot right
-    //Serial.print(next_direction);
-    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-    //delay(TURN_DURATION);
-  } 
-  else if (next_direction == 3) {
-    // move the robot down
-    //Serial.print(next_direction);
-    motors.setSpeeds(REVERSE_SPEED,REVERSE_SPEED);
-    //delay(REVERSE_DURATION);
-  } 
-  else if (next_direction == 4) {
-    // move the robot left
-    //Serial.print(next_direction);
+//robot stops 
+void stop(){
+    motors.setSpeeds(0, 0);
+    delay(1000);
+}
+
+//robot makes 90 degree left turn
+void turnLeft(){
     motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-    //delay(TURN_DURATION);
+    delay(TURN_LEFT_DURATION);
+    if (position == 'n'){
+    position = 'o';
+    }
+    else if (position == 'o'){position = 's';}
+    else if(position == 's'){position = 'e';}
+    else if(position == 'e'){position = 'n';}
+    stop();
+}
+
+//robot makes 90 degree right turn
+void turnRight(){
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    delay(TURN_RIGHT_DURATION);
+    if (position == 'n'){
+    position = 'e';
+    }
+    else if (position == 'e'){position = 's';}
+    else if(position == 's'){position = 'o';}
+    else if(position == 'o'){position = 'n';}
+    stop();
+}
+
+int count = 0;
+
+void loop() {
+  // 
+  if(count == 0){
+    for(int i = 0; i < PATH_LEN -1 ; i++){
+      int currentX =  path[i][0];
+      int currentY = path[i][1];
+
+      if(i == PATH_LEN -1){
+        // goal reached
+        break;
+      }
+      int nextX = path[i+1][0];
+      int nextY = path[i+1][1];
+
+      goPath(currentX, currentY, nextX, nextY);
+      
+    }
+    //ends loop
+    count++;
   }
-  else{
-    //Serial.println('stuck');
-  }
-  
 
 }
